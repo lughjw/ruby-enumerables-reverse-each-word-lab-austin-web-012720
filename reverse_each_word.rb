e12de60e2197f8a97e7218dc1d21ce69f3d5e1c2
Lugh Wilson <lughjw@gmail.com>
2020-01-03 18:05:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
-  str_array = str
+  str_array = str.collect { |string| str_array << string}
+  ret_array = []
+  str_array.each{ |string| ret_array << string.reverse }
   
-  str_array.each{ |string| str_array << string.reverse }
-  
-  str_array.to_s
+  ret_array.to_s
 end
